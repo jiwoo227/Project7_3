ffdb3e5ade269c80670a1b2f4299dd783e0f3738
최지우 <w2040@e-mirim.hs.kr>
2021-07-26 11:06:12 +0900

    edit toast.xml (Dialog.positiveButton Listener)7-3 commit

--- app/src/main/java/com/example/project7_3/MainActivity.java
+++ app/src/main/java/com/example/project7_3/MainActivity.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textName, textEmail;
+    EditText editMainName, editMainEmail;
+    TextView textToast;
     View dialogV;
     EditText editName, editEmail;
+    View toastV;
 
 
     @Override
@@ -21,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textName = findViewById(R.id.user_name);
-        textEmail = findViewById(R.id.user_email);
+        editMainName = findViewById(R.id.user_name);
+        editMainEmail = findViewById(R.id.user_email);
         Button btn_input = findViewById(R.id.btn_input);
 
         btn_input.setOnClickListener(btnListener);
@@ -41,11 +44,21 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(DialogInterface dialogInterface, int i) {
                     editName = dialogV.findViewById(R.id.dialog_edit_name);
                     editEmail = dialogV.findViewById(R.id.dialog_edit_email);
-                    textName.setText(editName.getText().toString());
-                    textEmail.setText(editEmail.getText().toString());
+                    editMainName.setText(editName.getText().toString());
+                    editMainEmail.setText(editEmail.getText().toString());
                 }
             });
-            dialog.setNegativeButton("취소", null);
+            dialog.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast toast = new Toast(MainActivity.this);
+                    toastV= View.inflate(MainActivity.this, R.layout.toast1, null);
+                    textToast = toastV.findViewById(R.id.toast_text);
+                    textToast.setText("취소했습니다.");
+                    toast.setView(toastV);
+                    toast.show();
+                }
+            });
             dialog.show();
         }
     };
